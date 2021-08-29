55eb08d368801ee17ffc9873a63ce325cb537494
pbatt <pbattican@gmail.com>
2021-08-29 00:21:14 -0400

    Initial JCK_DC defines

---
+++ common/defines/JCK_DC_defines.lua
@@ -0,0 +1,29 @@
+--- Defines for the DC tech tree / combat conversion ---
+
+--- Country
+NDefines.NCountry.BASE_RESEARCH_SLOTS = 3
+
+--- Production
+NDefines.NProduction.MAX_EQUIPMENT_RESOURCES_NEED = 4
+
+--- Technology
+NDefines.NTechnology.BASE_RESEARCH_POINTS_SAVED = 25.0
+NDefines.NTechnology.BASE_YEAR_AHEAD_PENALTY_FACTOR = 3
+NDefines.NTechnology.BASE_TECH_COST = 80
+NDefines.NTechnology.DEFAULT_XP_RESEARCH_BONUS = 0.5
+
+--- NMilitary
+NDefines.NMilitary.MAX_DIVISION_BRIGADE_WIDTH = 5			-- Max width of regiments in division designer.
+NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 4		-- Max height of regiments in division designer.
+NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2			-- Max width of support in division designer.
+NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 4		-- Max height of support in division designer.
+
+NDefines.NMilitary.LAND_EQUIPMENT_BASE_COST = 15					-- Cost in XP to upgrade a piece of equipment one level is base + ( total levels * ramp )
+NDefines.NMilitary.LAND_EQUIPMENT_RAMP_COST = 15					
+NDefines.NMilitary.NAVAL_EQUIPMENT_BASE_COST = 15
+NDefines.NMilitary.NAVAL_EQUIPMENT_RAMP_COST = 15
+NDefines.NMilitary.AIR_EQUIPMENT_BASE_COST = 15
+NDefines.NMilitary.AIR_EQUIPMENT_RAMP_COST = 15
+
+NDefines.NMilitary.BASE_COMBAT_WIDTH = 120                   -- base combat width
+NDefines.NMilitary.ADDITIONAL_COMBAT_WIDTH = 60                 -- more opened up by support attack
