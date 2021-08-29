693b89edb089cf7831b7bc01ffcc42044a5b1cf2
pbatt <pbattican@gmail.com>
2021-08-29 13:31:36 -0400

    Update defines for better playability - resources + template cost

--- common/defines/JCK_DC_defines.lua
+++ common/defines/JCK_DC_defines.lua
@@ -1,5 +1,8 @@
 --- Defines for the DC tech tree / combat conversion ---
 
+--- NBuildings 
+NDefines.NBuildings.INFRASTRUCTURE_RESOURCE_BONUS = 0.075
+
 --- Country
 NDefines.NCountry.BASE_RESEARCH_SLOTS = 3
 
@@ -17,6 +20,10 @@ NDefines.NMilitary.MAX_DIVISION_BRIGADE_WIDTH = 5			-- Max width of regiments in
 NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 4		-- Max height of regiments in division designer.
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2			-- Max width of support in division designer.
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 4		-- Max height of support in division designer.
+
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 8 	--Base cost to unlock a regiment slot,
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 4	--Base cost to change a regiment column.
+NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 6 	--Base cost to unlock a support slot
 
 NDefines.NMilitary.LAND_EQUIPMENT_BASE_COST = 15					-- Cost in XP to upgrade a piece of equipment one level is base + ( total levels * ramp )
 NDefines.NMilitary.LAND_EQUIPMENT_RAMP_COST = 15					
